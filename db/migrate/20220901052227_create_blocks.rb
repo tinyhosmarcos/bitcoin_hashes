c4d285ad9423b036f51250c62db9a1e15e6dae66
Yhostin Ollachica <yhostin.ollachica@ucsp.edu.pe>
2022-09-01 00:24:16 -0500

    CHORE: Set initial database for blocks

--- db/migrate/20220901052227_create_blocks.rb
+++ db/migrate/20220901052227_create_blocks.rb
@@ -1,11 +1,11 @@
 class CreateBlocks < ActiveRecord::Migration[7.0]
   def change
     create_table :blocks do |t|
-      t.string :hash_id
-      t.string :prev_block
-      t.integer :block_index
-      t.integer :time
-      t.integer :bits
+      t.string :hash_id, null: false
+      t.string :prev_block, null: false
+      t.integer :block_index, null: false
+      t.integer :time, null: false
+      t.integer :bits, null: false
 
       t.timestamps
     end
