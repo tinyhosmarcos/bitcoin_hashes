e21b2dba9e18e6704671ef3a80bde0a5c31e40a2
Yhostin Ollachica <yhostin.ollachica@ucsp.edu.pe>
2022-09-01 00:39:49 -0500

    CHORE: Added validations to block model

--- app/models/block.rb
+++ app/models/block.rb
@@ -1,2 +1,8 @@
 class Block < ApplicationRecord
+  # Validations
+  validates :hash_id, presence: true, uniqueness: true
+  validates :prev_block, presence: true
+  validates :block_index, presence: true
+  validates :time, presence: true
+  validates :bits, presence: true
 end
