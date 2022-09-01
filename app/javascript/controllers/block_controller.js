22795bf3b51968e15349205da76394202f8635cb
Yhostin Ollachica <yhostin.ollachica@ucsp.edu.pe>
2022-09-01 01:08:01 -0500

    CHORE: Added submit method to block controller without validations

--- app/javascript/controllers/block_controller.js
+++ app/javascript/controllers/block_controller.js
@@ -1,7 +1,21 @@
 import { Controller } from "@hotwired/stimulus"
+import apiFetch from "../services/api-fetch";
 
 // Connects to data-controller="block"
 export default class extends Controller {
-  connect() {
-  }
+  static targets = [ "hash" ]
+  hash_data_url = "https://blockchain.info/rawblock"
+  // connect() {
+  // }
+
+  async submit() {	
+    const hash_data = await apiFetch(this.hash_data_url, this.hashTarget.value, { method: "GET" });
+    // destructuring hash_data to hash, prev_block, block_index, time, bits
+    const { hash, prev_block, block_index, time, bits } = hash_data;
+    const response = await apiFetch("", "blocks", {
+      headers: { "X-CSRF-Token": document.querySelector("meta[name='csrf-token']").content },
+      method: "POST",
+      body: { hash_id: hash, prev_block: prev_block, block_index: block_index, time: time, bits: bits }
+    })
+  };
 }
