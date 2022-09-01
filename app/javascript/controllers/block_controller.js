4f8998eeeef3fb02c9bb59ce4f2a23bc3b7da8a9
Yhostin Ollachica <yhostin.ollachica@ucsp.edu.pe>
2022-09-01 03:17:58 -0500

    DELETE: Erase method delete

--- app/javascript/controllers/block_controller.js
+++ app/javascript/controllers/block_controller.js
@@ -5,17 +5,18 @@ import apiFetch from "../services/api-fetch";
 export default class extends Controller {
   static targets = [ "hash" ]
   hash_data_url = "https://blockchain.info/rawblock"
-  // connect() {
-  // }
-
+  
   async submit() {	
-    const hash_data = await apiFetch(this.hash_data_url, this.hashTarget.value, { method: "GET" });
+    const hash_data = await apiFetch(this.hash_data_url, this.hashTarget.value, false, { method: "GET" });
     // destructuring hash_data to hash, prev_block, block_index, time, bits
     const { hash, prev_block, block_index, time, bits } = hash_data;
-    const response = await apiFetch("", "blocks", {
+    const response = await apiFetch("", "blocks", true, {
       headers: { "X-CSRF-Token": document.querySelector("meta[name='csrf-token']").content },
       method: "POST",
       body: { hash_id: hash, prev_block: prev_block, block_index: block_index, time: time, bits: bits }
     })
-  };
+    console.log(response);
+    await Turbo.renderStreamMessage(response);
+  }
+
 }
